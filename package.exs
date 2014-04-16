c3db20522e660d70402c3e0995c0ea5b00657bc2
notmad <heh@lolnub.com>
2014-04-16 06:03:57 -0600

    Add package.exs config.

---
+++ package.exs
@@ -0,0 +1,8 @@
+Expm.Package.new(
+  name: "ilvmx",
+  description: "ILvMx (Internet Load virtual Module exchange)",
+  version: "0.1",
+  keywords: ["Elixir","Erlang","ilvmx","cloud", "app", "web", "framework"], 
+  maintainers: [[name: "lolnub.com developers", email: "heh@lolnub.com"]],
+  repositories: [[github: "lolnub/ilvmx", tag: "0.1"]]
+)
