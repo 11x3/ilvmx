250860764b94883367743c54f993099cdb4994f8
notmad <heh@lolnub.com>
2014-04-16 13:21:52 -0600

    Remove dead code. Fix tests.

--- lib/castle_supervisor.ex
+++ lib/castle_supervisor.ex
@@ -1,19 +1,20 @@
-defmodule ILM.BotLabSupervisor do
+defmodule Castle.Supervisor do
   use Supervisor.Behaviour
-  
-  # gen_supervisor
-  
+
   def start_link do
-    :supervisor.start_link(__MODULE__, [])
+    :supervisor.start_link({:local, :castle}, __MODULE__, [])
   end
 
-  def init(_) do
-    # todo: add a poolboy here
-    
+  def init([]) do
     children = [
       # Define workers and child supervisors to be supervised
-      worker(ILM.BotLab, [])
+      worker(Castle,  []),
+      worker(ILM.NubspaceSupervisor,  []),
     ]
+
+    # start our http server
+    # todo: check ILM.config before starting
+    Plug.Adapters.Cowboy.http ILM.WebServer, []
 
     # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
     # for other strategies and supported options
