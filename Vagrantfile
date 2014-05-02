cda9ba011e4aaf1f03cc6dc7797dcb897148d279
notmad <heh@lolnub.com>
2014-05-02 10:08:47 -0600

    Add mostly well rounded vagrant support.

--- Vagrantfile
+++ Vagrantfile
@@ -2,22 +2,39 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-
+  VAGRANTFILE_API_VERSION = "2"
+  
+  # Securing Vagrant as found:
+  # http://stackoverflow.com/questions/14715678/vagrant-insecure-by-default
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'" # avoids 'stdin: is not a tty' error.
+  config.ssh.private_key_path = ["#{ENV['HOME']}/.ssh/id_rsa","#{ENV['HOME']}/.vagrant.d/insecure_private_key"]
+  config.vm.provision "shell", inline: <<-SCRIPT
+    printf "%s\n" "#{File.read("#{ENV['HOME']}/.ssh/id_rsa.pub")}" > /home/vagrant/.ssh/authorized_keys
+    chown -R vagrant:vagrant /home/vagrant/.ssh
+  SCRIPT
+  
   # Box
   config.vm.box       = "hashicorp/precise64"
   
   # Providers
-   
   config.vm.provider "virtualbox" do |v|
     v.memory = 1024
     v.cpus = 2
   end
-
+  
+  config.vm.provider "rackspace" do |rs|
+    rs.username           = ENV["RS_USERNAME"] || (throw "nil ENV['RS_USERNAME']")
+    rs.api_key            = ENV["RS_API_KEY"]  || (throw "nil ENV['RS_API_KEY']")
+    rs.flavor             = /1 GB Performance/
+    rs.rackspace_region   = "dfw"
+    rs.server_name        = "ilvmx"
+  end
+  
   # Shared folders
   config.vm.synced_folder ".", "/ilvmx"
   
   # Network
-  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 8080, host: 8888
   
   # Custom
   config.vm.provision :shell, :path   => "vagrant.sh"
--- vagrant.sh
+++ vagrant.sh
@@ -1,17 +1,65 @@
 #!/usr/bin/env bash
+# Pull this file down, make it executable and run it with sudo
+# wget https://raw.github.com/gist/5487621/build-erlang-r16b.sh
+# chmod u+x build-erlang-r16b.sh
+# sudo ./build-erlang-r16b.sh
 
-# Install Erlang/Elixir
-apt-get update --fix-missing
-apt-get install -q -y cowsay python-software-properties python g++ make git curl libncurses5-dev
-wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-dpkg -i erlang-solutions_1.0_all.deb
-apt-get update && apt-get install -q -y erlang
+if [ $(id -u) != "0" ]; then
+  echo "You must be the superuser to run this script" >&2
+  exit 1
+fi
 
+# Setup
 cd /
+apt-get update --fix-missing
+
+# Install the build tools (dpkg-dev g++ gcc libc6-dev make)
+apt-get install -q -y cowsay build-essential git curl
+apt-get install -q -y python-software-properties python
+
+# automatic configure script builder (debianutils m4 perl)
+apt-get -y install autoconf
+
+# Needed for HiPE (native code) support, but already installed by autoconf
+# apt-get -y install m4
+ 
+# Needed for terminal handling (libc-dev libncurses5 libtinfo-dev libtinfo5 ncurses-bin)
+apt-get -y install libncurses5-dev
+ 
+# For building with wxWidgets
+apt-get -y install libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3
+ 
+# For building ssl (libssh-4 libssl-dev zlib1g-dev)
+apt-get -y install libssh-dev
+ 
+# ODBC support (libltdl3-dev odbcinst1debian2 unixodbc)
+apt-get -y install unixodbc-dev
+
+# Install Erlang
+mkdir -p /erlang
+cd /erlang
+
+if [ -e otp_src_17.0.tar.gz ]; then
+  echo "Good! 'otp_src_17.0.tar.gz' already exists. Skipping download."
+else
+  wget http://www.erlang.org/download/otp_src_17.0.tar.gz
+fi
+ 
+tar -xvzf otp_src_17.0.tar.gz
+chmod -R 777 otp_src_17.0
+cd otp_src_17.0
+./configure
+make
+make install
+
+# Install Elixir
+cd /
+
 git clone git://github.com/elixir-lang/elixir.git
 git clone git://github.com/rebar/rebar.git
+
 cd /rebar && make 
-cd /elixir && git checkout v0.13.0 && make && make install 
+cd /elixir && git checkout master && make && make install 
 
 ln -s /rebar/rebar /usr/local/bin/rebar
 
@@ -22,20 +70,15 @@ ln -s /rebar/rebar /usr/local/bin/rebar
 # cabal install elm
 # cabal install elm-server   # Highly-Recommended
 
-# [hack] 4/30/14 elm-get is failing due to pandoc failing
+# [bug] 4/30/14 elm-get is failing due to pandoc dep failing
 # cabal install pandoc
 # cabal install elm-repl     # Optional
 # cabal install elm-get      # Optional
-
-# # Config the app.
-# cd /ilvmx && mix clean --all
-
-# todo: fix con_cache/exactor deps
 
 # Install fish
 # wget http://fishshell.com/files/2.1.0/linux/Ubuntu/fish_2.1.0-1~precise_amd64.deb
 # sudo dpkg -i fish_2.1.0-1~precise_amd64.deb
 # chsh -s /usr/bin/fish
 
-
-mix deps.get && mix compile && mix test
+# # Config the app.
+cd /ilvmx && mix deps.get && mix compile && mix test && mix server
