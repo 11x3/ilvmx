c5b3655f1232d480caf394b6200505cdb64b2c0c
notmad <heh@lolnub.com>
2014-05-02 12:00:38 -0600

    Update config port for production.

--- castle/config.ex
+++ castle/config.ex
@@ -1,14 +1,14 @@
 defmodule ILM.Config do
   use Phoenix.Config.App
 
-  config :router, port: 8080
+  config :router, port: System.get_env("ILVMX_PORT") || 8080
   config :plugs, code_reload: false, consider_all_requests_local: false
 end
 
 defmodule ILM.Config.Dev do
   use ILM.Config
 
-  config :router, port: System.get_env("PORT") || 8080
+  config :router, port: System.get_env("ILVMX_PORT") || 8080
   config :plugs,  code_reload: true
   config :logger, level: :debug
 end
